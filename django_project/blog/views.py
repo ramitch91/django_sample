88c642ebf5412eafa18f53e900639d9b9abd449d
Ricky Mitchell <ramitch91@gmail.com>
2022-06-05 21:05:59 -0500

    add db and data

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,26 +1,10 @@
 from django.shortcuts import render
+from .models import Post
 
-
-# Create your views here.
-
-posts = [
-    {
-        'author': 'Ricky Mitchell',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'May 24, 2022'
-    },
-    {
-        'author': 'Amy Mitchell',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'May 25, 2022'
-    }
-]
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
