a72e9bb2d62bb0f1b23d5b80d327a1f49d095374
Ricky Mitchell <ramitch91@gmail.com>
2022-06-12 16:14:56 -0500

    add user registration form and crispy forms for formatting

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -34,6 +34,8 @@ if not DEBUG:
 
 INSTALLED_APPS = [
     'blog.apps.BlogConfig',
+    'users.apps.UsersConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -119,6 +121,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
