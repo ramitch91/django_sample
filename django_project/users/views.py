7072b5cb98e41e246943dda15291144dc11541a1
Ricky Mitchell <ramitch91@gmail.com>
2022-06-12 20:52:22 -0500

    add a profile page and require login to view

--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .forms import UserRegisterForm
 
 
@@ -15,3 +16,8 @@ def register(request):
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required
+def profile(request):
+    return render(request, 'users/profile.html')
